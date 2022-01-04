ced0ebea64165a97ab967a12e68208a3289c98c8
Venci <venci34@gmail.com>
2022-01-04 19:04:21 +0200

    done

--- src/js/Application.js
+++ src/js/Application.js
@@ -8,18 +8,23 @@ export default class Application extends EventEmitter {
     };
   }
 
+  count = 0;
+
   constructor() {
     super();
 
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
 
+    this._beat = new Beat();
+    this._beat.on(Beat.events.BIT, () => this._create(lyrics, this.count));
+  }
+  _create(lyrics) {
     const message = document.createElement("div");
     message.classList.add("message");
-    message.innerText = "Ah";
-
+    message.innerText = lyrics[this.count];
+    this.count++;
+    this.count = this.count >= lyrics.length ? 0 : this.count;
+    console.log(this.count);
     document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
   }
 }
